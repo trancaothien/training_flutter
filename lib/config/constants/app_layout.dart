e14feb3b36672d3c0a3f352c89607a93fa3b6d00
Trần Cao Thiên <thiennh278612@gmail.com>
2022-05-30 00:45:00 +0700

    change

---
+++ lib/config/constants/app_layout.dart
@@ -0,0 +1,12 @@
+class AppLayout {
+  AppLayout._();
+  static const borderLv1 = 6.0;
+  static const borderLv2 = 12.0;
+  static const borderLv3 = 20.0;
+  static const borderLv4 = 24.0;
+  static const borderLv5 = 32.0;
+
+  static const padding = 20.0;
+
+  static const buttonHeight = 48.0;
+}
--- lib/config/constants/constants.dart
+++ lib/config/constants/constants.dart
@@ -4,4 +4,4 @@ export 'app_styles.dart';
 export 'app_icons.dart';
 export 'app_themes.dart';
 export 'app_values.dart';
-export 'app_defind.dart';
+export 'app_layout.dart';
